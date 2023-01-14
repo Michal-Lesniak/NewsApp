4795e5779e08c9a7d31ecdfcafadbad714cd39de
Michal Lesniak <michalles1@interia.pl>
2023-01-14 13:10:15 +0100

    Wymodelowanie graficzne

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component , ViewChild, AfterViewInit, ChangeDetectorRef} from '@angular/core';
+import { MatSidenav} from '@angular/material/sidenav';
+import { BreakpointObserver} from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'NewsApp';
+  @ViewChild(MatSidenav) sid!: MatSidenav;
+
+  constructor(private observer: BreakpointObserver, private cdr: ChangeDetectorRef){}
+
+  ngAfterViewInit(): void {
+    this.sid.opened = true;
+    this.observer.observe(['(max-width: 787px)']).subscribe(
+      (res) =>  {
+        if(res.matches){
+            this.sid.mode = 'over';
+            this.sid.close();
+        }
+        else{
+          this.sid.mode = 'side';
+          this.sid.open();
+        }
+      }
+    );
+    this.cdr.detectChanges();
+  }
+
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatCardModule } from '@angular/material/card';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +17,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatDividerModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
